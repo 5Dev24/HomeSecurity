e4d4a0ea5270f070d8803cf1e12e485583140e8e
5Dev24 <omega524dev@gmail.com>
2019-09-17 10:22:38 -0500

    Fixed improper formatting of file and use of commands

--- Firmware/start.bat
+++ Firmware/start.bat
@@ -1,4 +1,4 @@
 @choice /c yn /t 5 /d y /m "Is Server?"
-@if ERRORLEVEL 1 python __main__.py -server True
-@if ERRORLEVEL 2 python __main__.py -server False
+@if ".%ERRORLEVEL%." == ".1." python __main__.py -server True
+@if ".%ERRORLEVEL%." == ".2." python __main__.py -server False
 @pause > nul
