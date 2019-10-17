6e98ee2a4748027451f054481d756b883458accc
5Dev24 <omega524dev@gmail.com>
2019-10-17 10:30:26 -0500

    Added file reading, line reading, file deleting, and a new handler for opening a file

--- Firmware/src/file.py
+++ Firmware/src/file.py
@@ -1,4 +1,4 @@
-from os import access as hasPerm
+from os import remove, access as hasPerm
 from os import R_OK, W_OK, X_OK, F_OK
 
 class FileType:
@@ -19,22 +19,41 @@ class File:
 	def canRead(self): return hasPerm(self._file, R_OK)
 	def canExecute(self): return hasPerm(self._file, X_OK)
 
-	def _write(self, data, mode):
+	def __file(self, mode, func = None, args = (), kwargs = {}):
 		f = None
 		try:
 			f = open(self._file, mode)
+			return func(f, *args, **kwargs)
+		except: return False
+		finally:
+			if f != None: f.close()
+
+	def _write(self, data, mode):
+		def __write(f, data):
 			f.write(data)
-		except: pass
-		finally: if f != None: f.close()
+		return self.__file(mode, __write, args = (data))
 
 	def override(self, data: str = None):
 		if data is None or type(data) != str or not self.exists() or not self.canWrite(): return
-		f = open(self._file, "w")
-		f.write(data)
-		f.close()
+		return self._write(data, "w")
 
 	def write(self, data: str = None):
 		if data is None or type(data) != str or not self.exists() or not self.canWrite(): return
-		f = open(self._file, "a")
-		f.write(data)
-		f.close()
+		return self._write(data, "a")
+
+	def readAll(self):
+		if not self.exists() or not self.canRead(): return None
+		def __read(f):
+			return f.readlines()
+		return self.__file("r", __read)
+
+	def readLine(self, line: int = 0):
+		if not self.exists() or not self.canRead(): return None
+		allLines = self.readAll()
+		if allLines is list and line < len(allLines): return allLines[line - 1]
+		else: return None
+
+	def delete(self):
+		if not self.exists(): return False
+		remove(self._file)
+		return not self.exists()
