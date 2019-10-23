a3984f1b14faeeffd805466f0abeef48fc58a3e6
5Dev24 <omega524dev@gmail.com>
2019-10-23 10:08:09 -0500

    Got file handling working for a default file and list files

--- Firmware/src/file.py
+++ Firmware/src/file.py
@@ -22,11 +22,12 @@ class File:
 	def canRead(self): return hasPerm(self._file, R_OK)
 	def canExecute(self): return hasPerm(self._file, X_OK)
 
-	def __file(self, mode, func = None, args = (), kwargs = {}):
+	def _fileInstance(self, mode, func = None, args = (), kwargs = {}):
 		f = None
 		try:
 			f = open(self._file, mode)
-			return func(f, *args, **kwargs)
+			if func is None: return False
+			else: return func(f, *args, **kwargs)
 		except: return False
 		finally:
 			if f != None: f.close()
@@ -36,15 +37,26 @@ class File:
 			try: f.write(data)
 			except: return False
 			return True
-		return self.__file(mode, __write, args = (data,))
+		return self._fileInstance(mode, __write, args = (data,))
+
+	def delete(self):
+		if not self.exists(): return False
+		remove(self._file)
+		return not self.exists()
+
+	def create(self):
+		if not self.exists(): self._fileInstance("w+", None)
+		return self.exists()
+
+class DefaultFile(File):
 
 	def overwrite(self, data: str = None):
-		if data is None or type(data) != str or not self.exists() or not self.canWrite(): return
-		return self._write(data, "w")
+		if data is None or type(data) != str or not super().exists() or not super().canWrite(): return
+		return super()._write(data, "w")
 
 	def write(self, data: str = None):
-		if data is None or type(data) != str or not self.exists() or not self.canWrite(): return
-		return self._write(data, "a")
+		if data is None or type(data) != str or not super().exists() or not super().canWrite(): return
+		return super()._write(data, "a")
 
 	def overwriteln(self, data: str = None):
 		return self.overwrite(data + "\n")
@@ -53,22 +65,50 @@ class File:
 		return self.write(data + "\n")
 
 	def readAllLines(self):
-		if not self.exists() or not self.canRead(): return None
+		if not super().exists() or not super().canRead(): return None
 		def __read(f):
 			return [l.replace("\n", "") for l in f.readlines()]
-		return self.__file("r", __read)
+		return super()._fileInstance("r", __read)
 
 	def readLine(self, line: int = 0):
-		if not self.exists() or not self.canRead(): return None
+		if not super().exists() or not super().canRead(): return None
 		allLines = self.readAllLines()
 		if type(allLines) is list and line <= len(allLines) and line > 0: return allLines[line - 1]
 		else: return None
 
-	def delete(self):
-		if not self.exists(): return False
-		remove(self._file)
-		return not self.exists()
+class ListFile(File):
 
-	def create(self):
-		if not self.exists(): self.__file("w+", None)
-		return self.exists()
+	def writeList(self, data: list = None):
+		def _write(file, data):
+			try:
+				if type(data) != list: return False
+				for element in data:
+					if element != None: file.write(str(element).replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t").replace("\b", "\\b") + "\n")
+					else: return False
+				return True
+			except Exception: return False
+		if type(data) != list: return False
+		return super()._fileInstance("w", _write, args = (data,))
+
+	def readList(self):
+		def _read(file):
+			try: return [l.replace("\n", "").replace("\\n", "\n").replace("\\r", "\r").replace("\\t", "\t").replace("\\b", "\b") for l in file.readlines()]
+			except Exception: return False
+		return super()._fileInstance("r", _read)
+
+	def addElement(self, element = None):
+		tmp = self.readList()
+		if not tmp or tmp is None: tmp = []
+		tmp.append(element)
+		self.writeList(tmp)
+
+	def clear(self):
+		self.writeList([])
+
+class Dictionary(File):
+
+	def writeDictionary(self, data: dict = None): pass
+	def readDictionary(self): pass
+	def addKey(self, key = None, value = None): pass
+	def overwriteKey(self, key = None, value = None): pass
+	def removeKey(self, key = None): pass
