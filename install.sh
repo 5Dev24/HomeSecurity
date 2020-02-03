934f7011b53c6bd49522cf3fced00b20f6035b34
5Dev24 <omega524dev@gmail.com>
2020-02-02 19:44:19 -0600

    Removed excess sudos

--- install.sh
+++ install.sh
@@ -13,23 +13,23 @@ StartFile=/home/pi/ISM-2019-2020/Firmware/start.sh
 DataFolder=/home/pi/ISM-2019-2020/Firmware/data/
 
 # Update system
-sudo apt update
-sudo apt -y upgrade
+apt update
+apt -y upgrade
 
 # Make sure python 3.x is installed
-sudo apt -y install python3
+apt -y install python3
 
 # Install pip so we can install requirements
-sudo apt -y install python3-pip
+apt -y install python3-pip
 
 # Install pybluez linux dependancies
-sudo apt -y install pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev
+apt -y install pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev
 
 # Install firmware's modules
-sudo pip3 install -r /home/pi/ISM-2019-2020/Firmware/requirements.txt
+pip3 install -r /home/pi/ISM-2019-2020/Firmware/requirements.txt
 
 # Clear up excess packages
-sudo apt -y autoremove
+apt -y autoremove
 
 # Get if this is a server or a client
 echo "Is this a server or a client?"
@@ -44,18 +44,18 @@ esac
 # Check if service file exists, if it does: delete it
 if [ -f "$ServiceFile" ]
 then
-	sudo chmod 777 "$ServiceFile"
-	sudo rm -f "$ServiceFile"
+	chmod 777 "$ServiceFile"
+	rm -f "$ServiceFile"
 fi
 
 # Create service file
-sudo touch "$ServiceFile"
+touch "$ServiceFile"
 
 # Allow all access
-sudo chmod 777 "$ServiceFile"
+chmod 777 "$ServiceFile"
 
 # Write to file
-sudo echo "[Unit]
+echo "[Unit]
 Description=ISM-2019-2020
 
 [Service]
@@ -65,26 +65,26 @@ ExecStart=$StartFile
 WantedBy=multi-user.target">> "$ServiceFile"
 
 # Set permissions back
-sudo chmod 644 "$ServiceFile"
+chmod 644 "$ServiceFile"
 
 # Tell systemd to start during boot
-sudo systemctl daemon-reload
-sudo systemctl enable ISM-2019-2020.service
+systemctl daemon-reload
+systemctl enable ISM-2019-2020.service
 
 # Get new device id
 read -p "Device ID: " DeviceID
 
 # Delete old data folder
-sudo rm -rf "$DataFolder"
+rm -rf "$DataFolder"
 
 # Create data folder
-sudo mkdir "$DataFolder"
+mkdir "$DataFolder"
 
 # Make start.sh file executable
-sudo chmod +x "$StartFile"
+chmod +x "$StartFile"
 
 # Start start.sh
-sudo "$StartFile --install -id '$DeviceID' -server $Server"
+"$StartFile --install -id '$DeviceID' -server $Server"
 
 # Restart system
-sudo reboot
+reboot
